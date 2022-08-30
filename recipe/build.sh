b05ffd6526e76d16b7c20d675790babf54ba05dc
Mark Kittisopikul <markkitt@gmail.com>
2022-08-30 00:44:40 -0400

    Modify activate.sh and add documentation

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,11 +4,13 @@ set -euxo pipefail
 
 ${PYTHON} -m pip install . -vv
 
+# Build an accessory Julia depot containing all of
+# SymbolicRegression.jl's dependencies
+
 # Start a fake Julia depot
 FAKEDEPOT="${PREFIX}/share/SymbolicRegression.jl/fake_depot"
 export JULIA_DEPOT_PATH="${FAKEDEPOT}"
 # Set the JULIA_PROJECT so PyCall.jl gets installed into it
-# FIXME: Get version correctly
 export JULIA_PROJECT="@pysr-${PKG_VERSION}"
 mkdir -p "${FAKEDEPOT}"
 ${PYTHON} -c 'import pysr; pysr.install();'
@@ -17,10 +19,19 @@ ${PYTHON} -c 'import pysr; pysr.install();'
 # into the real depot that we will package
 SRDEPOT="${PREFIX}/share/SymbolicRegression.jl/depot"
 mkdir -p "${SRDEPOT}"
+
+# Select particular depot subdirectories to package
+# Julia package source code and potentially deps.jl
 mv "${FAKEDEPOT}/packages" "${SRDEPOT}/packages"
+# Binary dependencies including openspecfun and LLVMExtra
 mv "${FAKEDEPOT}/artifacts" "${SRDEPOT}/artifacts"
+# Julia environment pysr-${VERSION} with PyCall.jl, SymbolicRegression.jl
+# and ClusterManagers.jl
 mv "${FAKEDEPOT}/environments" "${SRDEPOT}/environments"
+# The conda directory should only contain a single file,
+# deps.jl, which configures Conda.jl
 mv "${FAKEDEPOT}/conda" "${SRDEPOT}/conda"
+
 # Destroy the fake depot
 rm -rf "${FAKEDEPOT}"
 export JULIA_DEPOT_PATH="${SRDEPOT}"
@@ -32,3 +43,8 @@ do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
     cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
+
+# Activate the versioned pysr julia project upon activate.
+# The activated Projewct.toml should contain PyCall.jl, SymbolicRegression.jl,
+# and ClusterManagers.jl
+"export JULIA_PROJECT=\"@pysr-${PKG_VERSION}\"" >> "${PREFIX}/etc/conda/activate.d/${PKG_NAME}_activate.sh"
--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,3 +1,5 @@
-export JULIA_DEPOT_PATH="${JULIA_DEPOT_PATH}:${CONDA_PREFIX}/share/SymbolicRegression.jl/depot"
-# FIXME: get PySR version
-export JULIA_PROJECT="@pysr-0.10.1"
+# This should execute after julia_activate.sh
+# Assumes that the JULIA_DEPOT_PATH ends in a colon
+export JULIA_DEPOT_PATH="${JULIA_DEPOT_PATH}${CONDA_PREFIX}/share/SymbolicRegression.jl/depot:"
+
+# build.sh will insert a line to set JULIA_PROJECT to pysr-${VERSION}
