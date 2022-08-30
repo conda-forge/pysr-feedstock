c4c913bbc7399b6b654666406a389464e9aadec8
Mark Kittisopikul <markkitt@gmail.com>
2022-08-29 23:37:25 -0400

    Fix activate.sh: PREFIX to CONDA_PREFIX

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,3 +1,3 @@
-export JULIA_DEPOT_PATH="${JULIA_DEPOT_PATH}:${PREFIX}/share/SymbolicRegression.jl/depot"
+export JULIA_DEPOT_PATH="${JULIA_DEPOT_PATH}:${CONDA_PREFIX}/share/SymbolicRegression.jl/depot"
 # FIXME: get PySR version
 export JULIA_PROJECT="@pysr-0.10.1"
