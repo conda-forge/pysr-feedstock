56bba72238d327c965b2f7435b34ff5a66deb5bf
Mark Kittisopikul <markkitt@gmail.com>
2022-08-29 20:15:06 -0400

    Create a SymbolicRegression.jl depot for conda packaging

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,30 @@ set -euxo pipefail
 
 ${PYTHON} -m pip install . -vv
 
-julia -e "using Pkg; Pkg.add(\"PyCall\")"
+# Start a fake Julia depot
+FAKEDEPOT="${PREFIX}/share/SymbolicRegression.jl/fake_depot"
+export JULIA_DEPOT_PATH="${FAKEDEPOT}"
+# Set the JULIA_PROJECT so PyCall.jl gets installed into it
+# FIXME: Get version correctly
+export JULIA_PROJECT="@pysr-0.10.1"
+mkdir -p "${FAKEDEPOT}"
+${PYTHON} -c 'import pysr; pysr.install();'
 
-${PYTHON} -c 'import pysr; pysr.install()'
+# Copy packages, artifacts, environments, and conda dirs
+# into the real depot that we will package
+SRDEPOT="${PREFIX}/share/SymbolicRegression.jl/depot"
+mkdir -p "${SRDEPOT}"
+mv "${FAKEDEPOT}/packages" "${SRDEPOT}/packages"
+mv "${FAKEDEPOT}/artifacts" "${SRDEPOT}/artifacts"
+mv "${FAKEDEPOT}/environments" "${SRDEPOT}/environments"
+mv "${FAKEDEPOT}/conda" "${SRDEPOT}/conda"
+# Destroy the fake depot
+rm -rf "${FAKEDEPOT}"
+export JULIA_DEPOT_PATH="${SRDEPOT}"
+
+# Report on the build
+ls -r ${PREFIX}/share/SymbolicRegression.jl/depot
+cat ${PREFIX}/share/SymbolicRegression.jl/depot/environments/*
 
 # # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # # This will allow them to be run on environment activation.
