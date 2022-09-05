9baabe329888f6b2fa02d57618d8ad28a3a09a1e
Mark Kittisopikul <markkitt@gmail.com>
2022-09-05 17:45:09 -0400

    Remove JULIA_SSL_CA_ROOTS_PATH modifications
    julia-feedstock now incorporates these changes

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -9,12 +9,6 @@ export PYSR_PROJECT_BACKUP=${PYSR_PROJECT:-}
 # Assumes that the JULIA_DEPOT_PATH ends in a colon
 export JULIA_DEPOT_PATH="${JULIA_DEPOT_PATH}${CONDA_PREFIX}/share/pysr/depot:"
 
-# For testing purposes to see if we can macos tests to pass
-# See https://github.com/conda-forge/julia-feedstock/pull/221
-if [[ $OSTYPE == 'darwin'* ]]; then
-  export JULIA_SSL_CA_ROOTS_PATH=""
-fi
-
 # build.sh will insert lines to set JULIA_PROJECT to @pysr-${VERSION},
 # to insert that into the project stack via JULIA_LOAD_PROJECT, ans
 # set PYSR_PROJECT
