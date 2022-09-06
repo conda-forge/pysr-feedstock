c318ba442d66cd04d9b8d6be5bef4dc73c4979cc
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-09-06 19:43:52 -0400

    Cleanup scripts

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
 export JULIA_DEPOT_PATH_PYSR_BACKUP=${JULIA_DEPOT_PATH:-}
-export JULIA_PROJECT_PYSR_BACKUP=${JULIA_PROJECT:-}
-export JULIA_LOAD_PATH_PYSR_BACKUP=${JULIA_LOAD_PATH:-}
-export PYSR_PROJECT_BACKUP=${PYSR_PROJECT:-}
 
 # This should execute after julia_activate.sh
 # Assumes that the JULIA_DEPOT_PATH ends in a colon
 export JULIA_DEPOT_PATH="${JULIA_DEPOT_PATH}${CONDA_PREFIX}/share/pysr/depot:"
-
-# THE FOLLOWING ACTION HAS BEEN DISABLED IN BUILD.SH
-# build.sh may insert lines to set JULIA_PROJECT to @pysr-${VERSION},
-# to insert that into the project stack via JULIA_LOAD_PROJECT, ans
-# set PYSR_PROJECT
